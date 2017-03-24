e320b96b416d821194e4a7dc7c73a37a773b1aa0
Christopher Davenport <chris@christopherdavenport.tech>
2017-03-23 20:56:58 -0400

    Fix Formatting

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ module "vpc" {
 
   name = "${var.name}"
 
-  cidr = "10.1.0.0/16"
-  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
-  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24"]
+  cidr             = "10.1.0.0/16"
+  private_subnets  = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets   = ["10.1.101.0/24", "10.1.102.0/24"]
   database_subnets = ["10.1.201.0/24", "10.1.202.0/24"]
 
   # Disabled NAT Gateway means internal nets cannot access the outside internet.
   enable_nat_gateway = false
 
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs = ["us-east-1a", "us-east-1b"]
 
   tags = "${var.tags}"
 }
